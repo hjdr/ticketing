e9209933bbf4fa905cecea268c66258d17151202
hjdr <harry.jdr@gmail.com>
2021-10-07 16:17:11 +0100

    add optimistic concurrency control for tickets service

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   price: number;
@@ -10,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
   price: number;
   title: string;
   userId: string;
+  version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -37,7 +39,8 @@ const ticketSchema = new mongoose.Schema({
     }
   },
 });
-
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 ticketSchema.statics.build = (attrs: TicketAttrs) => new Ticket(attrs)
 
 // create model
